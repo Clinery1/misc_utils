9dfeb47b4be112853b0e187b9337569f15c62619
Clinery1 <clinery8237@gmail.com>
2024-04-25 03:42:03 -0400

    Bump version with breaking changes Change the API of `SlotMap` to return options instead of panicing, and add a "reserve" method for inserting a blank spot. Also add the method to update the blank spot in-place and make the rest of the code easier with the `Slot<T>` enum

--- src/slotmap.rs
+++ src/slotmap.rs
@@ -6,14 +6,85 @@ use std::ops::{
 use crate::Key;
 
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+enum Slot<T> {
+    Value(T),
+    Reserved,
+    None,
+}
+impl<T> Slot<T> {
+    pub fn take(&mut self)->Option<T> {
+        match std::mem::replace(self, Self::None) {
+            Self::Value(t)=>Some(t),
+            _=>None,
+        }
+    }
+
+    #[inline]
+    pub fn insert(&mut self, data: T) {
+        *self = Self::Value(data);
+    }
+
+    pub fn is_reserved(&self)->bool {
+        match self {
+            Self::Reserved=>true,
+            _=>false,
+        }
+    }
+
+    pub fn has_data(&self)->bool {
+        match self {
+            Self::Value(_)=>true,
+            _=>false,
+        }
+    }
+
+    pub fn as_ref(&self)->Option<&T> {
+        match self {
+            Self::Value(t)=>Some(t),
+            _=>None,
+        }
+    }
+
+    pub fn as_mut(&mut self)->Option<&mut T> {
+        match self {
+            Self::Value(t)=>Some(t),
+            _=>None,
+        }
+    }
+}
+
+
 /// A simple map of key:value that reuses old keys that are removed. DOES NOT solve the ABA
 /// problem. The user (me) assumes all responsibility to ensure all keys are used properly.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SlotMap<K: Key, T> {
-    inner: Vec<Option<T>>,
+    inner: Vec<Slot<T>>,
     free: Vec<K>,
 }
 impl<K: Key, T> SlotMap<K, T> {
+    fn get_slot(&mut self)->K {
+        let k;
+        if let Some(key) = self.free.pop() {
+            k = key;
+        } else {
+            k = K::from_id(self.inner.len());
+            self.inner.push(Slot::Reserved);
+        }
+
+        return k;
+    }
+
+    /// if the key points to a reserved slot or some data we are storing
+    fn is_key_valid(&self, k: &K)->bool {
+        if k.id() < self.inner.len() {
+            let d = &self.inner[k.id()];
+            d.has_data() || d.is_reserved()
+        } else {
+            false
+        }
+    }
+
     pub fn new()->Self {
         SlotMap {
             inner: Vec::new(),
@@ -22,49 +93,62 @@ impl<K: Key, T> SlotMap<K, T> {
     }
 
     pub fn insert(&mut self, data: T)->K {
-        let key = self.free.pop().unwrap_or(K::from_id(self.inner.len()));
-        self.inner[key.id()] = Some(data);
+        let key = self.get_slot();
+        self.inner[key.id()].insert(data);
 
         return key;
     }
 
-    /// assumes the key is valid
-    pub fn get(&self, key: K)->&T {
-        let id = key.id();
-        assert!(id < self.inner.len());
-        assert!(self.inner[id].is_some());
-
-        return self.inner[id].as_ref().unwrap();
+    #[inline]
+    pub fn reserve_slot(&mut self)->K {
+        self.get_slot()
     }
 
-    /// assumes the key is valid
-    pub fn get_mut(&mut self, key: K)->&mut T {
-        let id = key.id();
-        assert!(id < self.inner.len());
-        assert!(self.inner[id].is_some());
+    /// Returns Err(data) when the key DOES NOT point to a valid reserved entry.
+    pub fn insert_reserved(&mut self, key: K, data: T)->Result<(), T> {
+        if !self.is_key_valid(&key) {
+            return Err(data);
+        }
+        if !self.inner[key.id()].is_reserved() {
+            return Err(data);
+        }
 
-        return self.inner[id].as_mut().unwrap();
+        self.inner[key.id()].insert(data);
+
+        return Ok(());
     }
 
-    /// assumes the key is valid
-    pub fn remove(&mut self, key: K)->T {
-        let id = key.id();
-        assert!(id < self.inner.len());
-        assert!(self.inner[id].is_some());
+    pub fn get(&self, key: K)->Option<&T> {
+        if !self.is_key_valid(&key) {return None}
 
-        return self.inner[id].take().unwrap();
+        let id = key.id();
+        return self.inner[id].as_ref();
+    }
+
+    pub fn get_mut(&mut self, key: K)->Option<&mut T> {
+        if !self.is_key_valid(&key) {return None}
+
+        let id = key.id();
+        return self.inner[id].as_mut();
+    }
+
+    pub fn remove(&mut self, key: K)->Option<T> {
+        if !self.is_key_valid(&key) {return None}
+
+        let id = key.id();
+        return self.inner[id].take();
     }
 }
 impl<K: Key, T> Index<K> for SlotMap<K, T> {
     type Output = T;
     #[inline]
     fn index(&self, key: K)->&T {
-        self.get(key)
+        self.get(key).unwrap()
     }
 }
 impl<K: Key, T> IndexMut<K> for SlotMap<K, T> {
     #[inline]
     fn index_mut(&mut self, key: K)->&mut T {
-        self.get_mut(key)
+        self.get_mut(key).unwrap()
     }
 }
